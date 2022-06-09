2a793c87952f8d89cfc9d18df34e0d8635f6800e
MrBuryy <alexanderburiy02@yandex.ru>
2022-06-09 13:22:36 +0300

    done console command

--- routes/console.php
+++ routes/console.php
@@ -1,19 +1,14 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+
+Artisan::command('addressCount {id}', function ($id) {
+    $customer = \App\Models\Customer::where('id', $id)->with('address')->first();
+    if ($customer != null) {
+        $this->info("Customer with id {$id} has {$customer->address->count()} addresses");
+    } else {
+        $this->error("No such customer");
+    }
+})->purpose('Find out, how many addresses a customer has');
